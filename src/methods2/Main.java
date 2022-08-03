546d1109ed6307a06ff3e8ccd75cabdc780a1c33
ali <alialpay96@hotmail.com>
2022-08-03 16:19:53 +0300

    variable arguments

--- src/methods2/Main.java
+++ src/methods2/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println(yeniMesaj);
         int sayi = topla(5,7);
         System.out.println(sayi);
+        int toplam = topla2(5,6,5,3,4);
+        System.out.println(toplam);
     }
     public static void ekle(){
 
@@ -22,6 +24,14 @@ public class Main {
 
     public static int topla(int sayi1, int sayi2){
         return sayi1+sayi2;
+    }
+
+    public static int topla2(int... sayilar){
+        int toplam =0;
+        for(int sayi:sayilar){
+            toplam+=sayi;
+        }
+        return  toplam;
     }
 
     public static String sehirVer(){
