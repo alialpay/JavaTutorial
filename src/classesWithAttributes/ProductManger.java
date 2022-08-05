4f7254d6318887d468dec970ac9c93997e344549
Ali Alpay <alialpay96@hotmail.com>
2022-08-05 09:00:05 +0300

    classes

--- src/classesWithAttributes/ProductManger.java
+++ src/classesWithAttributes/ProductManger.java
@@ -3,7 +3,7 @@ package classesWithAttributes;
 public class ProductManger {
     public void Add(Product product){
         //JDBC
-        System.out.println("Ürün eklendi " + product.name);
+        System.out.println("Ürün eklendi " + product.getName());
     }
 
     public void Add2(){
