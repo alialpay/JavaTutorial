20d6fc4506d2f7efeed71723f58ccafb4bc9611a
Ali Alpay <alialpay96@hotmail.com>
2022-08-08 08:38:24 +0300

    abstractClasses

---
+++ src/arrayListDemo/Main.java
@@ -0,0 +1,23 @@
+package arrayListDemo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList sayilar = new ArrayList();
+        sayilar.add(1);
+        sayilar.add(10);
+        sayilar.add("Ankara");
+
+
+//        sayilar.remove(0);
+//        sayilar.set(0,"İstanbul");
+//        sayilar.clear();
+//        System.out.println(sayilar.get(0));
+
+        for(Object i :sayilar){
+            System.out.println(i);
+        }
+
+    }
+}
